99e38c43a223910ace67724e23539b7f4af4c33e
Jonas <publik.froeller.jonas@gmx.at>
2025-01-17 21:16:31 +0100

    fix: resource count placement not visible if window is resized and improve styling of it

--- UI/ui.gd
+++ UI/ui.gd
@@ -1,8 +1,10 @@
 extends CanvasLayer
 
-
-@onready var label: Label = $Label
-
+@onready var wood_label: Label = $ResourceContainer/WoodRow/WoodLabel
+@onready var stone_label: Label = $ResourceContainer/StoneRow/StoneLabel
+@onready var gold_label: Label = $ResourceContainer/GoldRow/GoldLabel
 
 func _process(_delta):
-	label.text = "Wood: " + str(Game.wood) + " Stone: " + str(Game.stone) + " Gold: " + str(Game.gold)
+	wood_label.text = "Wood: " + str(Game.wood)
+	stone_label.text = "Stone: " + str(Game.stone)
+	gold_label.text = "Gold: " + str(Game.gold)
