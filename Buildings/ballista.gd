fd62ed555507c170977b0ba569a85571a8399536
Jonas <publik.froeller.jonas@gmx.at>
2025-01-22 22:32:02 +0100

    improves ballista logic

--- Buildings/ballista.gd
+++ Buildings/ballista.gd
@@ -8,6 +8,7 @@ extends StaticBody2D
 # Internal variables
 var enemies_in_range = []
 var fire_timer: Timer
+var current_target = null
 
 func _ready():
 	# Initialize timer
@@ -18,41 +19,69 @@ func _ready():
 	fire_timer.connect("timeout", self._on_fire_timer_timeout)
 	add_child(fire_timer)
 
-func _process(delta):
+func _process(_delta):
 	update_enemies_in_range()
+	update_sprite_direction()
 
 func update_enemies_in_range():
 	enemies_in_range.clear()
 	for enemy in get_tree().get_nodes_in_group("enemies"):
 		if is_instance_valid(enemy):
-			if position.distance_to(enemy.global_position) <= 5000:
+			if position.distance_to(enemy.global_position) <= 200:
 				enemies_in_range.append(enemy)
+
+func update_sprite_direction():
+	current_target = get_nearest_enemy()
+	if current_target:
+		var direction = position.direction_to(current_target.global_position)
+		var angle = direction.angle()
+		
+		# Convert angle to sprite frame
+		# Normalize angle to 0-2PI range
+		angle = fmod(angle + 2 * PI, 2 * PI)
+		
+		# Select appropriate frame based on angle
+		if angle < PI/4 or angle > 7*PI/4:  # Right
+			$BallistaSprite.frame = 15
+		elif angle < 3*PI/4:  # Down
+			$BallistaSprite.frame = 8
+		elif angle < 5*PI/4:  # Left
+			$BallistaSprite.frame = 22
+		else:  # Up
+			$BallistaSprite.frame = 0
 
 func _on_fire_timer_timeout():
 	if enemies_in_range.size() > 0:
 		shoot_projectile()
 
 func shoot_projectile():
-	# Get the nearest enemy
-	var nearest_enemy = get_nearest_enemy()
-	if not nearest_enemy:
+	current_target = get_nearest_enemy()
+	if not current_target:
 		return
 	
 	var projectile = projectile_scene.instantiate()
-	var direction = position.direction_to(nearest_enemy.global_position)
-	add_child(projectile)
+	var direction = position.direction_to(current_target.global_position)
+	
+	var units_node = get_tree().get_root().get_node("World/Units")
+	units_node.add_child(projectile)
+	
+	# Offset the spawn position based on ballista sprite
+	var spawn_offset = Vector2(0, -16)
+	projectile.global_position = global_position + spawn_offset
+	
+	# Set bolt sprite frame and rotation
+	var angle = direction.angle()
+	projectile.get_node("Sprite2D").frame = 1 if abs(angle) > PI/2 else 0
 	projectile.rotation = direction.angle()
-	projectile.global_position = global_position
-	# Give the projectile velocity
-	projectile.velocity = direction * projectile_speed
-	projectile.rotation = direction.angle()
+	projectile.velocity = direction * projectile.speed
 
 func get_nearest_enemy() -> Node2D:
 	var nearest = null
 	var shortest_distance = INF
 	for enemy in enemies_in_range:
-		var distance = position.distance_to(enemy.position)
-		if distance < shortest_distance:
-			shortest_distance = distance
-			nearest = enemy
+		if is_instance_valid(enemy):
+			var distance = position.distance_to(enemy.position)
+			if distance < shortest_distance:
+				shortest_distance = distance
+				nearest = enemy
 	return nearest
--- Entities/bolt.gd
+++ Entities/bolt.gd
@@ -1,19 +1,40 @@
 extends CharacterBody2D
 
 @export var lifetime: float = 5.0
+@export var damage: int = 10
+@export var speed: float = 400.0
 
 func _ready():
+	print("Bolt spawned")
 	var timer = Timer.new()
 	timer.wait_time = lifetime
 	timer.one_shot = true
-	timer.connect("timeout", self.queue_free)
+	timer.connect("timeout", self._on_timer_timeout)
 	add_child(timer)
 	timer.start()
 
-func _physics_process(delta):
-	move_and_slide()
-
-func _on_body_entered(body):
-	if body.name == "Peasant":
-		body.apply_damage(10) # Call enemy's damage function
+func _physics_process(_delta):
+	var collision = move_and_collide(velocity * _delta)
+	if collision:
+		var collider = collision.get_collider()
+		if collider.is_in_group("enemies"):
+			print("Direct collision with enemy")
+			collider.apply_damage(damage)
 		queue_free()
+
+func _on_area_2d_body_entered(body):
+	print("Area entered by: ", body.name)
+	if body.is_in_group("enemies"):
+		print("Hit enemy: ", body.name)
+		body.apply_damage(damage)
+		queue_free()
+
+func _on_area_2d_area_entered(area):
+	print("Bolt hit area: ", area.get_parent().name)
+	var parent = area.get_parent()
+	if parent.is_in_group("enemies"):
+		parent.apply_damage(damage)
+		queue_free()
+
+func _on_timer_timeout():
+	queue_free()
